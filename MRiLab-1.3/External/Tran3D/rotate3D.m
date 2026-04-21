4e65a9cc0f3a46f0b6a524af417bd841e6079c07
wanglihui <wanglh2022@shanghaitech.edu.cn>
2026-04-21 19:08:04 +0800

    删除mrilab

--- MRiLab-1.3/External/Tran3D/rotate3D.m
+++
@@ -1,8 +0,0 @@
-
-function Tr_Im=rotate3D(Im, T, R)
-% rotate 3D volume by T & R
-
-[row,col,layer] = size(Im);
-Tr_Im = tformarray(Im, T, R, [1 2 3], [1 2 3], [row, col,layer], [], []);
-
-end
--- MRiLab-1.3/Lib/bin/ReadMe.txt
+++
@@ -1,1 +0,0 @@
-MEX file folder
